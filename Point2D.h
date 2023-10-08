06019928e93fe7d61633c86145c1dcf37c8ae078
Carlos Mira Lopez <carlosnds2000@gmail.com>
2023-10-08 13:43:33 +0200

    Finalizado el Point2D

---
+++ Point2D.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+
+
+class Point2D{
+	public:
+		double x;
+		double y;
+		
+		Point2D(double x=0, double y=0){
+			this -> x = x;
+			this -> y = y;
+		
+		}
+		
+		static double distance(const Point2D &a, const Point2D &b){
+		
+			return sqrt(pow(a.x - b.x,2)+pow(a.y - b.y,2));
+		
+		}
+
+		friend bool operator==(const Point2D &a, const Point2D &b){
+		
+			if(a.x == b.x && a.y == a.y){
+			
+				return true;
+			}
+			return false;
+		
+		}
+
+		friend bool operator!=(const Point2D &a, const Point2D &b){
+		
+			if(a.x != b.x && a.y != b.y){
+			
+				return true;
+			}
+
+			return false;
+		}
+
+		friend std::ostream& operator<<(std::ostream &out, const Point2D &p){
+			
+			out << "(" << p.x << ", " << p.y << ")" << std::endl;
+			return out;
+		
+		}
+
+
+
+
+};
