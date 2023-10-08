cc14ae4135f77eabb97b4e5237299531d0107470
Carlos Mira Lopez <carlosnds2000@gmail.com>
2023-10-08 12:54:02 +0200

    acabado el listlinked

--- ListLinked.h
+++ ListLinked.h
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <exception>
 #include "List.h"
+#include "Node.h"
 template <typename T>
 
 class ListLinked:public List<T>{
@@ -8,41 +10,124 @@ class ListLinked:public List<T>{
 		int n;
 	public:
 		//Metodos heredados
-		virtual void insert(int pos, T e) = 0;
-		virtual void append( T e) = 0;
-		virtual void prepend( T e) = 0;
+		virtual void insert(int pos, T e){
+		
+			if(pos<0 || pos>size()){
+				throw std::out_of_range("Posicion invalida");
+			
+			}
+		
+			if(pos==0){
+				prepend(e);
+			}else if(pos==n){
+				append(e);
+			}else{
+				Node<T>* aux = first->next;
+				Node<T>* nuevo=new Node<T>(e,nullptr);
+				Node<T>* prev = first;
+
+				for(int x=0;x<size();x++){
+					if(x == pos-1 ){
+						prev->next = nuevo;
+						nuevo->next = aux;
+						n++;
+						break;
+					}
+					prev=aux;
+					aux=aux->next;	
+				}
+			}
+		
+		}
+
+		virtual void append( T e){
+			Node<T>* aux = first->next;
+			Node<T>* prev= first;
+			while(aux!=nullptr){
+				prev=aux;
+				aux=aux->next;
+			}
+			prev->next = new Node<T>(e,nullptr);
+			n++;
+
+		}
+
+		virtual void prepend( T e){
+			Node<T>* aux = first;
+			Node<T>* nuevo=new Node<T>(e,nullptr);
+			first = nuevo;
+			first ->next = aux;
+			n++;
+		}
+
 		virtual T remove(int pos){
-		
-		
+			if(pos<0 || pos >(size()-1)){
+				throw std::out_of_range("Posicion invalida");
+			
+			}
+
+			Node<T>* aux = first;
+			Node<T>* prev = nullptr;
+			for(int x=0; x<size();x++){
+				if(x==pos&&pos==0){
+					
+					first = first->next;
+					n--;
+					return aux->data;
+
+				}else if(x==pos && pos ==size()-1){
+					
+					prev->next =nullptr;
+					n--;
+					return aux->data;
+
+				}else if(x==pos){
+					
+					prev->next = aux->next;
+					n--;
+					return aux->data;
+					
+				}
+				prev=aux;
+				aux = aux->next;
+				
+				
+			
+			}
+
+			return -1;
 		}
 
 		virtual T get(int pos){
 			if(pos>n){
-				throw out_of_range("Posicion invalida");
+				throw std::out_of_range("Posicion invalida");
 			}
-			Node<T> aux=first;
+			Node<T>* aux=first;
 			for( int x=0;x<n;x++){
 				if(x==pos){
-					return aux.data;
+
+					return aux->data;
 				}
 				aux=aux->next;
 			
 			}
-			return nullptr;
+		
+			return -1;
 		
 		}
 
 		virtual int search( T e){
-			Node<T> aux=first;
+			Node<T>* aux=first;
 			for(int x=0;x<n;x++){
-				if(aux.data == e){
+				if(aux->data == e){
+					
 					return x;
 				}
 				aux=aux->next;
 		
 			}
-
-			return -1
+			
+			return -1;
 		
 		
 		}
@@ -52,6 +137,7 @@ class ListLinked:public List<T>{
 			if(n==0){
 				return true;
 			}
+			return false;
 		}
 
 		virtual int size(){
@@ -60,39 +146,46 @@ class ListLinked:public List<T>{
 
 		//Metodos de la clase
 		ListLinked(){
+			
 			first=nullptr;
 			n=0;
 		}
 		~ListLinked(){
-			Node<T> aux;
-			for(int x=n;x>0;x--){
-			aux=first->next;
-			n--;
-			delete first;
-			first=aux
+			Node<T>* aux=first->next;
+			while(aux!=nullptr){
+				delete first;
+				first = aux;
+				aux = aux->next;
 			}
+
+			delete first;
+			
 		}
 		T operator[](int pos){
 			if(pos>n){
-				throw out_of_range("fuera de rango");
+				throw std::out_of_range("fuera de rango");
 			}
-			Node<T> aux=first;
-			for(x=0;x<n;x++){
-				if(n==pos){
-					return aux.data;
+			Node<T>* aux=first;
+			for(int x=0;x<n;x++){
+				if(x==pos){
+					return aux->data;
 				}
-				aux=aux.next;
+				aux=aux->next;
 			}
-			return nullptr;
+			return -1;
 		
 			
 		
 		}
 		
 		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-			for(ListLinked p : list){
-				out<<p.data<<std::endl;
+			Node<T>* aux=list.first;
+			out<<"-->["<<std::endl;	
+			while(aux!=nullptr){
+				out<< aux->data <<std::endl;
+				aux=aux->next;
 			}
+			out<<"]"<<std::endl;	
 			return out;
 		}
 
--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ bin/testnode: TestNode.cpp Node.h
 	mkdir -p bin
 	g++ -o bin/TestNode TestNode.cpp Node.h
 
-
+bin/testlinked: testListLinked.cpp ListLinked.h List.h Node.h
+	mkdir -p bin
+	g++ -o bin/testlinked testListLinked.cpp ListLinked.h Node.h
 clean:
 	rm -r *.o *gch bin
